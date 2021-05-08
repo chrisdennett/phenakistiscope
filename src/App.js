ebb666aabedb82816a8b608e57e76415ea4b6afd
chrisdennett <chrisdennett@gmail.com>
2021-05-08 10:55:38 +0100

    Set up site to show scopes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import DiscPlayer from "./components/discPlayer/DiscPlayer";
+import { Leva, useControls } from "leva";
 
 function App() {
+  const controlValues = useControls({
+    isSpinning: true,
+    msPerFrame: { value: 70, min: 0, max: 400 },
+    size: { value: 400, min: 50, max: 800 },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Leva collapsed="true" />
+      <h1>Gardner Family Phenakistoscopes</h1>
+      <div className="discs">
+        {discs.map((d, i) => (
+          <DiscPlayer key={i} {...d} {...controlValues} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const discs = [
+  { url: "./img/disc-01.jpg", center: { x: 0.502, y: 0.492 } },
+  { url: "./img/disc-02.jpg", center: { x: 0.5, y: 0.5 } },
+  { url: "./img/disc-03.jpg", center: { x: 0.5, y: 0.5 } },
+  { url: "./img/disc-04.jpg", center: { x: 0.5, y: 0.5 } },
+  { url: "./img/disc-05.jpg", center: { x: 0.5, y: 0.5 } },
+  { url: "./img/disc-06.jpg", center: { x: 0.5, y: 0.5 } },
+  { url: "./img/disc-07.jpg", center: { x: 0.5, y: 0.5 } },
+];
---
+++ src/components/discPlayer/DiscPlayer.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from "react";
+import useInterval from "../../hooks/useInterval";
+import styles from "./discPlayer.module.css";
+
+export default function DiscPlayer({
+  url,
+  center,
+  isSpinning,
+  msPerFrame,
+  size,
+}) {
+  const [sourceImg, setSourceImg] = useState(null);
+  const [, setRotation] = useState(0);
+  const canvasRef = useRef(null);
+
+  const segments = 10;
+  const segAngle = 360 / segments;
+
+  useInterval(() => {
+    if (!sourceImg) return;
+
+    setRotation((prev) => {
+      let newRot = prev + segAngle;
+      if (newRot > 360) {
+        const overlap = 0; //newRot - 360;
+        newRot = segAngle + overlap;
+      }
+
+      // for testing
+      if (!isSpinning) newRot = 0;
+
+      drawRotatedCircle(canvasRef.current, sourceImg, newRot, center, size);
+
+      return newRot;
+    });
+  }, msPerFrame);
+
+  useEffect(() => {
+    if (!sourceImg) {
+      const image = new Image();
+      image.crossOrigin = "Anonymous";
+      image.onload = () => {
+        setSourceImg(image);
+      };
+      image.src = url;
+    }
+  }, [url, sourceImg, size]);
+
+  return (
+    <div className={styles.discPlayer}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
+}
+
+const drawRotatedCircle = (canvas, image, degrees, center, size) => {
+  const { width: inputW, height: inputH } = image;
+
+  const wToHRatio = inputH / inputW;
+
+  const outWidth = size;
+  const outHeight = outWidth * wToHRatio;
+
+  canvas.width = outWidth;
+  canvas.height = outHeight;
+
+  const ctx = canvas.getContext("2d");
+  const circleRadius = canvas.width / 2;
+
+  const canvasMiddle = { x: center.x * outWidth, y: center.y * outHeight };
+
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(canvasMiddle.x, canvasMiddle.y, circleRadius, 0, Math.PI * 2);
+  ctx.clip();
+
+  ctx.translate(canvasMiddle.x, canvasMiddle.y);
+  ctx.rotate(-(degrees * Math.PI) / 180);
+  ctx.translate(-canvasMiddle.x, -canvasMiddle.y);
+  ctx.drawImage(image, 0, 0, inputW, inputH, 0, 0, outWidth, outHeight);
+  ctx.restore();
+
+  // ctx.beginPath();
+  // ctx.fillStyle = "rgba(0,255,255,0.7)";
+  // ctx.arc(canvasMiddle.x, canvasMiddle.y, 10, 0, Math.PI * 2);
+  // ctx.fill();
+};
